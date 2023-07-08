0cc1c9daf12cb0a0c4cbec33e96e52b1f6171e06
IvashkaVIP <ivashka.co@gmail.com>
2023-07-08 19:15:36 +0300

    add Searchbar comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,27 @@
-import { startTransition } from 'react';
 import css from './App.module.css';
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
 
-export const App = () => {
-  return <div className={css.App}>React homework template</div>;
+export class App extends Component  {
+  state = {
+    searchQuery: '',
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const query = evt.target[1].value.trim();
+    console.log(query);
+    if (!query) {
+      alert('field cannot be empty'); return;
+    }
+    this.setState({ searchQuery: query });
+        evt.target.reset();
+  }
+  
+  render() {
+    return (
+      <div className={css.App}>
+        <Searchbar handleQuery={this.handleSubmit}></Searchbar>
+      </div>
+    );}
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+import css from './Searchbar.module.css'
+
+export const Searchbar = ({ handleQuery }) => {
+
+        return (
+          <header className={css.Searchbar}>
+            <form className={css.SearchForm} onSubmit={handleQuery}>
+              <button type="submit">
+                <AiOutlineSearch className={css['SearchForm-button']} />
+                <span className={css['SearchForm-button-label']}>Search</span>
+              </button>
+
+              <input
+                className={css['SearchForm-input']}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder=" Search images and photos"
+              />
+            </form>
+          </header>
+        );
+  
+}
