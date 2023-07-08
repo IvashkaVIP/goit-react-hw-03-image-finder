9f35eb3481f2a3d94071b701be8aa49b2e8f2e5a
IvashkaVIP <ivashka.co@gmail.com>
2023-07-08 16:14:22 +0300

    creat folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
+import { startTransition } from 'react';
+import css from './App.module.css';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div className={css.App}>React homework template</div>;
 };
